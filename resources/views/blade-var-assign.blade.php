7d8ba059613a470d1d3aac1afacbaf26e46023f7
Alexei Raiu <alex.rayu@gmail.com>
2022-03-31 13:30:32 +0300

    Start creating the test environment.

---
+++ resources/views/blade-var-assign.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Oleksii Raiu - Blade and Twig comparison.</title>
+      <link rel="stylesheet" media="all" href="/css/app.css" />
+    </head>
+
+    <body>
+    <main class="container">
+      <h1>Oleksii Raiu - Blade and Twig comparison.</h1>
+      <p>This is a list of tested items. All samples run in a loop of 1 mln iterations.</p>
+
+      <section>
+        <h2>1.4.1 Variables. Assigning, printing.</h2>
+        <h3>Blade:</h3>
+        <ol>
+          <li><a href="/blade/variable-assign">Create a variable</a></li>
+        </ol>
+      </section>
+
+    </main>
+
+    </body>
+</html>
