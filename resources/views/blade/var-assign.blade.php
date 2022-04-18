e1afa2fe4d9a1adcdeadcf5a77388cadc9cf3721
Alexei Raiu <alex.rayu@gmail.com>
2022-04-18 13:00:20 +0300

    Complete variables handling.

--- resources/views/blade/var-assign.blade.php
+++ resources/views/blade/var-assign.blade.php
@@ -1,2 +1,8 @@
-@php($name = "Oleksii")
+{{-- COMMENT: Blade does not support assignment, uses raw PHP --}}
+@php($name = "John")
 Hello, {{ $name }}
+@php($user = ['name' => $name])
+Array name: {{ $user['name'] }}
+@php($user = new stdClass())
+@php($user->name = $name)
+Object name: {{ $user->name }}
